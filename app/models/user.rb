97f6995e0572298931425735ae71ecfbb3e74340
kent0322 <kent_0322@softbank.ne.jp>
2021-08-10 15:42:41 +0900

    ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,19 +9,17 @@ class User < ApplicationRecord
                    format: { with: VALID_PASSWORD_REGEX,
                     message: "は半角6~12文字英小文字・数字それぞれ１文字以上含む必要があります"}
 
-  validates :nickname, presence: true, length: { maximum: 20 }
-  validates :birthday, presence: true
+with_options presence: true do
+  validates :nickname, length: { maximum: 20 }
+  validates :birthday
 
-# 全角のひらがなor漢字を使用していないか検証
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "全角ひらがな、全角カタカナ、漢字で入力して下さい"} do
-    validates :last_name
+  with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "全角ひらがな、全角カタカナ、漢字で入力して下さい"} do
     validates :first_name
+    validates :last_name
   end
 
-  # 全角のカタカナを使用していないか検証
   with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/,message: "全角カタカナで入力して下さい"  }do
     validates :last_name_kana
     validates :first_name_kana
   end
 end
-
