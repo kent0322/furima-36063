f15a84deb5bc702104b20109b76dc2c0d98d24c1
kent0322 <kent_0322@softbank.ne.jp>
2021-08-12 14:10:10 +0900

    商品出品機能

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,12 +1,10 @@
 window.addEventListener('load', () => {
-  console.log("OK");
 
 const priceInput = document.getElementById("item-price");
-console.log(priceInput);
 
 priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
-  console.log(inputValue);
+
   const addTaxDom = document.getElementById("add-tax-price");
 addTaxDom.innerHTML = inputValue * 0.1;
 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -68,7 +68,7 @@ RSpec.describe Item, type: :model do
             end
 
             it "価格は、¥300~¥9,999,999の間のみ保存可能であること。" do
-                @item.price = "100"
+                @item.price = 100
                 @item.valid?
                 expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
             end
@@ -78,5 +78,51 @@ RSpec.describe Item, type: :model do
                 @item.valid?
                 expect(@item.errors.full_messages).to include("Price is not a number")
             end
+
+            it "カテゴリーに「---」が選択されている場合は出品できない" do
+                @item.category_id = "a"
+                @item.valid?
+                expect(@item.errors.full_messages).to include("Category can't be blank")
+            end
+            it "商品の状態に「---」が選択されている場合は出品できない " do 
+                @item.condition_id = "a"
+                @item.valid?
+                expect(@item.errors.full_messages).to include("Condition can't be blank")
+            end
+            it "配送料の負担に「---」が選択されている場合は出品できない" do
+                @item.load_id = "a"
+                @item.valid?
+                expect(@item.errors.full_messages).to include("Load can't be blank")
+            end
+            it "発送元の地域に「---」が選択されている場合は出品できない" do
+                @item.area_id = "a"
+                @item.valid?
+                expect(@item.errors.full_messages).to include("Area can't be blank")
+            end
+            it "発送までの日数に「---」が選択されている場合は出品できない" do
+                @item.days_id = "a"
+                @item.valid?
+                expect(@item.errors.full_messages).to include("Days can't be blank")
+            end
+            it "半角英数混合では登録できないこと" do
+                @item.price = "asdg"
+                @item.valid?
+                expect(@item.errors.full_messages).to include()
+            end
+            it "半角英語だけでは登録できないこと" do
+                @item.price = 123
+                @item.valid?
+                expect(@item.errors.full_messages).to include()
+            end
+            it "価格が9_999_999円を超えると出品できない" do
+                @item.price = 10000000
+                @item.valid?
+                expect(@item.errors.full_messages).to include()
+            end
+            it "userが紐付いていなければ出品できない" do
+                @item.user_id = nil
+                @item.valid?
+                expect(@item.errors.full_messages).to include()
+            end
         end
-end
+    end
